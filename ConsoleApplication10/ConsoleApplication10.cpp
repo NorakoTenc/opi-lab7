123d999dd42b783a0dc48b9e452a301f756535f4
Norako_ <worlhentai69@outlook.com>
2021-10-22 14:57:17 +0300

    first commit

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include "math.h"
+#include "windows.h"
+
+using namespace std;
+//comment in Visual studio
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	double f, a, x, c, b;
+	cout << "Уведіть a: ";
+	cin >> a;
+	cout << "Уведіть x ";
+	cin >> x;
+	cout << "Уведіть c: ";
+	cin >> c;
+	cout << "Уведіть b ";
+	cin >> b;
+	if (x < 6 && b + c != 0 && c!=0) {
+		f = (a * pow(x, 3)) / c + b;
+	}
+	else {
+		if (x > 6 && b + c == 0 && b!=0) {
+			f = a * pow(x, 2) - c / b;
+		}
+		else {
+			f = x / (-c + 2); 
+		}
+	}
+	printf("F= %lf", f);
+}
